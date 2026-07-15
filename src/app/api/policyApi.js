1dda9dd846322c2a0857aee215e981fc21ec640e
alihamza-12 <alihamzaarshad12@gmail.com>
2026-07-15 11:52:30 +0500

    one fix

--- src/app/api/policyApi.js
+++ src/app/api/policyApi.js
@@ -1,9 +1,9 @@
 import { httpClient } from "./httpClient";
 
 export const getPolicyById = async (id) => {
-  return httpClient.get(`/api/policies/${id}`);
+  return httpClient.get(`/api/policies/${id}`); //  Corrected
 };
 
 export const getMyPolicies = async () => {
-  return httpClient.get(`/api/policies/my`);
+  return httpClient.get('/api/policies/my');   //  Corrected
 };
